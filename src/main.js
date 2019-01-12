259c740695c0742e9cafd9a5ef530394a89ed25a
Vlad <vladislavmoiseenkov@gmail.com>
2019-01-12 14:57:22 +0200

    init app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -8,5 +9,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App),
+  template: '<App></App>',
+  components: { App },
 }).$mount('#app');
